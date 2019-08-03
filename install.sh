54cc44f7c497bd2ce51073445f15eef6009f82fd
Mike Reinstein <mreinstein@sellpoints.com>
2019-08-03 10:54:22 -0700

    add nginx reverse proxy instructions

--- install.sh
+++ install.sh
@@ -11,6 +11,17 @@ sudo apt-get upgrade
 
 npm install
 
+
+# install and configurate nginx
+sudo apt-get install nginx
+
+sudo systemctl enable nginx
+
+sudo mv nginx-config /etc/nginx/sites-available/default
+
+sudo /etc/init.d/nginx reload
+
+
 # configure the voice interaction agent to run automatically at boot, restart on failure
 sudo cp desk.service /lib/systemd/system/
 
