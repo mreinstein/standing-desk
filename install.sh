3f8a4f043fad76189a713b4b7feada6a2cab1fd1
Mike Reinstein <reinstein.mike@gmail.com>
2019-08-04 11:24:48 -0700

    update node to v12

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install node/npm
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 sudo apt-get install -y nodejs build-essential
 
